fd8973167cbf2771fd024bf97a4fd5a967ff95d8
jeissoni <jeissoni_22@hotmail.com>
2022-02-16 16:59:17 -0500

    add parameter for deploy an verification

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,12 @@
 import { task } from "hardhat/config"
 import "@nomiclabs/hardhat-waffle";
-import "@nomiclabs/hardhat-ethers";
+import "@nomiclabs/hardhat-etherscan";
 
+require('dotenv').config()
 
+const projeId=process.env.INFURA_PROJECT_ID
+const privateKey = process.env.DEPLOYER_SIGNER_PRIVATE_KEY
+const etherscanApi = process.env.ETHERSCAN_API_KEY;
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
