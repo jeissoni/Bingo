d5eba7296772408c1193344c4ed02f01d56788eb
jeissoni <jeissoni_22@hotmail.com>
2022-02-17 17:51:25 -0500

    delete code not using

--- contracts/Bingo.sol
+++ contracts/Bingo.sol
@@ -1,27 +1,6 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
-import "hardhat/console.sol";
-import "@chainlink/contracts/src/v0.8/VRFConsumerBase.sol";
-
-contract Bingo is VRFConsumerBase {
-    bytes32 private s_keyHash;
-    uint256 private s_fee;
-
-    uint256 public randomResult;
-
-    mapping(bytes32 => address) private s_rollers;
-    mapping(address => uint256) private s_results;
-
-    constructor(
-        address vrfCoordinator,
-        address link,
-        bytes32 keyHash,
-        uint256 fee
-    ) VRFConsumerBase(vrfCoordinator, link) {
-        s_keyHash = keyHash;
-        s_fee = fee;
-    }
 
      function fulfillRandomness(bytes32 requestId, uint256 randomness) internal override {
         randomResult = randomness;
