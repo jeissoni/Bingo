d0b86c0bd4fc07a7667b2c86c2704a739b450a88
jeissoni <jeissoni_22@hotmail.com>
2022-02-17 17:52:09 -0500

    delete code not using

--- contracts/Bingo.sol
+++ contracts/Bingo.sol
@@ -5,14 +5,5 @@ import "./struct/strucs.sol";
 import "./owner/ownerFunction.sol";
 import "./utils/utils.sol";
 
-     function fulfillRandomness(bytes32 requestId, uint256 randomness) internal override {
-        randomResult = randomness;
     }
 
-    // constructor() 
-    //     VRFConsumerBase(
-    //         0xdD3782915140c8f3b190B5D67eAc6dc5760C46E9, // VRF Coordinator
-    //         0xa36085F69e2889c224210F603D836748e7dC0088  // LINK Token
-    //     )
-    // {
-}
