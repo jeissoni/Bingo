f8511d1ed5687690ca393bb89b47cb50027aadbd
jeissoni <jeissoni_22@hotmail.com>
2022-02-17 17:51:45 -0500

    import library using

--- contracts/Bingo.sol
+++ contracts/Bingo.sol
@@ -1,6 +1,9 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
+import "./struct/strucs.sol";
+import "./owner/ownerFunction.sol";
+import "./utils/utils.sol";
 
      function fulfillRandomness(bytes32 requestId, uint256 randomness) internal override {
         randomResult = randomness;
