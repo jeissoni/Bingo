b37a420e3f5724f75dde2ed4da5e491e68aa55fe
jeissoni <jeissoni_22@hotmail.com>
2022-02-18 17:54:14 -0500

    import new files

--- contracts/Bingo.sol
+++ contracts/Bingo.sol
@@ -2,8 +2,9 @@
 pragma solidity ^0.8.0;
 
 import "./struct/strucs.sol";
-import "./owner/ownerFunction.sol";
-import "./utils/utils.sol";
+import "./utils/Counters.sol";
+import "./token/IERC20.sol";
+
  
 
 contract Bingo {
