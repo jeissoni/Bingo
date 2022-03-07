d175a1b0d7013827dad0af6b6445f78820e9fcba
jeissoni <jeissoni_22@hotmail.com>
2022-03-07 18:00:21 -0500

    add scrpt for to deploy smart contract

--- scripts/scriptBingo.ts
+++ scripts/scriptBingo.ts
@@ -1,4 +1,18 @@
-import { ethers } from "ethers";
+import { ethers } from "hardhat";
 
-import {abi} from '../artifacts/contracts/Bingo.sol/Bingo.json'
+const deploy = async() => {    
+    const [deployer] = await ethers.getSigners();
+    console.log('Deploying contrat with the account: ', deployer.address)
+    const RandomNumberConsumerFactory = await ethers.getContractFactory("RandomNumberConsumer");
+    const RandomNumberConsumerFactoryDeploy = await RandomNumberConsumerFactory.deploy(249);
+   
+    console.log("RandomNumberConsumer isdeployed at:", RandomNumberConsumerFactoryDeploy.address )
+}
 
+deploy().then(()=> process.exit(0)).catch(error => {
+    console.log(error);
+    process.exit(1);
+});
+
+
+//0xF502DCCd41962d62B2f49D5342e4219812a64392
